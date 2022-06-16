179de4ffd1984e232c200f6427c6b755a1fc5cdb
TheRickster365 <83165129+TheRickster365@users.noreply.github.com>
2022-06-16 18:43:54 +1000

    Create sslChecker.ini.php

---
+++ sslChecker.ini.php
@@ -0,0 +1,15 @@
+[sslChecker]
+# When to turn orange. Turns red when less than 0
+warnDays = 30
+
+# PHP Date format to show the expiry date.
+dateFormat = Y-m-d
+
+# Example domains - Change these to be your own domains
+# Add port number if not on standrd 443 port eg www.foo.com:8080
+#
+domains[]="www.google.com"
+domains[]="www.microsoft.com"
+domains[]="www.yahoo.com"
+domains[]="www.facebook.com"
+domains[]="www.twitter.com"
